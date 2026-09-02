ef0006e4bba59fcd29f6c721cbebd835397c14d3
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2026-09-02 15:09:45 +0530

    Router DOM

--- FirstReact/src/App.jsx
+++ FirstReact/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import Header from './Component/header/Header'
 import Body from './Component/body/Body'
+import Register from './Component/body/Registration'
+import Student from './Component/body/Student'
 import Footer from './Component/footer/Footer'
 
 const App = () => {
   return (
-    <div className='container'>
-        <Header/>
-        <Body/>
-        <Footer/>
+    <div className="container">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/stud" element={<Student />} />
+      </Routes>
+
+      <Footer />
     </div>
   )
 }
