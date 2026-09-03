f69b3a8c9575ef7aa9f7d0b0479dc1be1e479534
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2026-09-03 09:42:08 +0530

    tailwind

--- FirstReact/src/Component/header/Header.jsx
+++ FirstReact/src/Component/header/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-        <h3>React JS</h3>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-primary-subtle">
+      <div className="container">
+
+        <NavLink
+          to="/"
+          className="navbar-brand fw-bold text-primary"
+        >
+          React JS
+        </NavLink>
+
+        <div className="navbar-nav ms-auto">
+          <NavLink to="/" className="nav-link px-3">
+            Home
+          </NavLink>
+
+          <NavLink to="/register" className="nav-link px-3">
+            Register
+          </NavLink>
+
+          <NavLink to="/counter" className="nav-link px-3">
+            Counter
+          </NavLink>
+        </div>
+
+      </div>
+    </nav>
   )
 }
 
