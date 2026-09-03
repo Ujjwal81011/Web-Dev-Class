5f3c74e4c7312620553d203c891843b7f2097472
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2026-09-03 10:05:15 +0530

    Error

--- FirstReact/src/Component/Error.jsx
+++ FirstReact/src/Component/Error.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-        <h1>404:page not found</h1>
+    <div className="text-center py-5 my-5">
+
+      <h1 className="display-1 fw-bold text-primary">
+        404
+      </h1>
+
+      <h2 className="fw-bold">
+        Page Not Found
+      </h2>
+
+      <p className="text-secondary mb-4">
+        Sorry, the page you are looking for doesn't exist.
+      </p>
+
+      <Link to="/" className="btn btn-primary px-4">
+        Go to Home
+      </Link>
+
     </div>
   )
 }
